7b5f5a99e45b8c9982ba9789dc2c4b52005d17ba
Denis <46033956+DenisShmyrev@users.noreply.github.com>
2024-01-14 00:59:39 +0300

    js for making "to do list"

---
+++ Задания 13.01.24/jscript.js
@@ -0,0 +1,44 @@
+let input = document.getElementById("input");
+let addButton = document.getElementById("add-button");
+let container = document.getElementById("list");
+
+let todos= JSON.parse(localStorage.getItem("todos")) ?? []; 
+// ?? = если слева undefined илии null, то он применит правую часть
+
+render();
+
+// добавить проверку для того чтобы в массив не добавлялись пустые значения, например "". Если в inputvalue пустая строга, то ничего не добавляется
+addButton.addEventListener("click", () => {
+  let inputValue = input.value;
+  if (inputValue===""){
+    alert("Строка ввода не должна быть пустой!");
+    return;
+    }
+  todos.push(inputValue);
+  render();
+  saveTolocalStorage();
+});
+
+container.addEventListener("click", (event) => {
+  let todoId = Number(event.target.getAttribute("data-id"));
+  todos.splice(todoId, 1);
+  render();
+  saveTolocalStorage();
+});
+
+function render() {
+  let newInnerHtml = todos.reduce((prev, current, index) => {   //начало
+    let newValue = `
+<div style="display: flex">
+<div>${current}</div>
+<button data-id="${index}">Удалить</button>
+</div>
+`;
+    return prev + newValue;
+  }, "");   //конец
+  container.innerHTML = newInnerHtml;
+}
+function saveTolocalStorage() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+    
